b716072c43f11677caf96a887b1afdc604163f1e
Vanesa Edo <vanesa.en@gmail.com>
2024-02-20 08:15:35 +0100

    1,2,3,4,5,6 Resueltos. Fallta el 7 que está en proceso.

--- scripts.js
+++ scripts.js
@@ -17,7 +17,27 @@ console.log(dominio);
 //5 
 
 let nodos = document.getElementsByTagName("img");
-console.log(nodos);
+for (let i = 0; i < 10; i++) {
+    console.log(nodos[i].alt);
+    console.log(nodos[i].src); 
+    console.log(nodos[i].class); 
+}
+
+//6: 6. Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+
+let sustSrc = document.getElementsByTagName("img");
+for (let i = 0; i < sustSrc.length; i++) {
+    sustSrc[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"; 
+}
+
+
+//7. Cambia el fondo de todos los `infocard-lg-data text-muted` para todos los Pokimon voladores `itype flying`
+
+let elementos = document.getElementsByTagName("class");
+while (elementos.class = ) {
+    
+}
+
 /*
 nodo.getElementsByClassName (<clase CSS>): con esta sentencia podemos seleccionar un nodo según el nombre de su clase CSS.
 
